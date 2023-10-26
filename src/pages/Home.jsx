103fa2e16e753a81f22218cc63e09cc8a8542d5f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-26 14:03:51 +0700

    font inter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import TodoList from "../components/TabsTodo/TodoList";
 function Home() {
   return (
     <>
-      <div className="font-inter flex h-full w-full flex-col items-center justify-center bg-black p-4">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-black p-4 font-inter">
         <div className="flex w-full max-w-3xl flex-col gap-4 border-black">
           <h1 className="text-2xl font-bold text-[#fafafa]">What's the plan</h1>
           <div className="flex gap-3">
             <input
-              className="place w-full rounded-lg border-[1px] border-gray-800 bg-transparent p-2 text-[#fafafa] ring-white focus:ring-2 "
+              className="w-full rounded-lg border-[1px] border-gray-800 bg-transparent p-2 text-[#fafafa] ring-white focus:ring-2 "
               type="text"
               placeholder="What todo"
             />

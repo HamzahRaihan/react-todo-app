3de6b3d0d86b68d85f305f6e40cffc5045e1dad1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-26 14:04:40 +0700

    refactor function filter todos

---
+++ src/utils/Utils.jsx
@@ -0,0 +1,11 @@
+export const filterTodos = (todos, tab) => {
+  if (tab === "all") {
+    return todos;
+  } else if (tab === "active") {
+    return todos.filter((todo) => !todo.isComplete);
+  } else if (tab === "completed") {
+    return todos.filter((todo) => todo.isComplete);
+  } else {
+    return [];
+  }
+};

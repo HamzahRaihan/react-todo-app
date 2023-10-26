363c0fbf0d5750be4d9b4bfaf0a79fb2899c5f31
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-26 18:48:59 +0700

    refactor edit function

--- src/components/TabsTodo/Edit.jsx
+++ src/components/TabsTodo/Edit.jsx
@@ -1,25 +1,39 @@
 import PropTypes from "prop-types";
-
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editTodoList } from "../../redux/slices/TodoSlice";
 function Edit(props) {
-  console.log("🚀 ~ file: Edit.jsx:2 ~ Edit ~ modal:", props);
+  const dispatch = useDispatch();
+
+  const handleEditTodoList = (id, title, isChecked) => {
+    dispatch(editTodoList(id, title, isChecked));
+  };
+  const [inputEdit, setInputEdit] = useState(props.todoEditInput);
 
   return (
     <>
-      <div className="fixed left-0 top-0 flex h-screen w-screen justify-center gap-2 backdrop-blur ">
-        <button
-          className="fixed right-[300px] top-10 h-10 w-12 rounded-lg bg-[#fafafa] px-2 transition-all hover:opacity-90 active:opacity-75"
-          onClick={() => props.setModal(false)}
-        >
-          X
-        </button>
-        <div className="flex w-[800px] items-center justify-center gap-4 ">
+      <div className="fixed left-0 top-0 flex h-screen w-screen  justify-center gap-2 backdrop-blur">
+        <div className="relative flex w-[800px] max-w-5xl items-center justify-center gap-4 p-10">
+          <button
+            className="absolute right-10 top-10 h-10 w-12 rounded-lg bg-[#fafafa] px-2 transition-all hover:opacity-90 active:opacity-75"
+            onClick={() => props.setModal(false)}
+          >
+            X
+          </button>
           <input
             className="w-full rounded-lg border-[1px] border-gray-800 bg-transparent p-2 text-[#fafafa] ring-white focus:ring-2 "
             type="text"
             placeholder="What todo"
-            value={props.todoEditInput}
+            value={inputEdit}
+            onChange={(e) => setInputEdit(e.target.value)}
           />
-          <button className="rounded-lg bg-[#fafafa] px-4 py-2 transition-all hover:opacity-90 active:opacity-75">
+          <button
+            className="rounded-lg bg-[#fafafa] px-4 py-2 transition-all hover:opacity-90 active:opacity-75"
+            onClick={() => {
+              handleEditTodoList(props.todoID, inputEdit, props.todoComplete);
+              props.setModal(false);
+            }}
+          >
             Edit
           </button>
         </div>
@@ -29,9 +43,12 @@ function Edit(props) {
 }
 
 Edit.propTypes = {
-  modal: PropTypes.boolean,
-  setModal: PropTypes.function,
+  modal: PropTypes.bool,
+  setModal: PropTypes.func,
   todoEditInput: PropTypes.string,
+  handleEditTodoList: PropTypes.func,
+  todoID: PropTypes.string,
+  todoComplete: PropTypes.bool,
 };
 
 export default Edit;

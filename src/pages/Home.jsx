5a94facfe3a12571be1ca8747cffde8e441cbf64
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-26 18:49:30 +0700

    added feature to add todos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import TodoList from "../components/TabsTodo/TodoList";
+import { addTodoList } from "../redux/slices/TodoSlice";
+import { useDispatch } from "react-redux";
 
 /* eslint-disable react/no-unescaped-entities */
 function Home() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      title: input,
+      isComplete: false,
+      createdAt: Date.now(),
+    };
+    dispatch(addTodoList(newTodo));
+  };
+
   return (
     <>
       <div className="flex h-full w-full flex-col items-center justify-center bg-black p-4 font-inter">
@@ -12,8 +27,13 @@ function Home() {
               className="w-full rounded-lg border-[1px] border-gray-800 bg-transparent p-2 text-[#fafafa] ring-white focus:ring-2 "
               type="text"
               placeholder="What todo"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <button className="rounded-lg bg-[#fafafa] px-4 transition-all hover:opacity-90 active:opacity-75">
+            <button
+              className="rounded-lg bg-[#fafafa] px-4 transition-all hover:opacity-90 active:opacity-75"
+              onClick={handleAddTodo}
+            >
               Add
             </button>
           </div>
